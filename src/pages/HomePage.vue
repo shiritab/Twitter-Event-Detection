8b79ccc491bca2681cb686017def9465e60f23fd
shiritab <69760048+shiritab@users.noreply.github.com>
2021-09-27 14:15:37 +0300

    initial dash, changed frontend to src

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1> Trending </h1>
+      <b-form-select v-model="selected" :options="algorithms"></b-form-select>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "HomePage",
+    props: {
+        algorithms:{
+            type: Array,
+            required: true
+        }
+    },
+    data(){
+        return{
+            selected: null,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+// import Main from "./pages/MainPage";
+// import NotFound from "./pages/NotFoundPage";
+
+const routes = [
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   component: Home
+  // },
+  // {
+  //   path: "*",
+  //   name: "notFound",
+  //   component: NotFound
+  // }
+];
+
+export default routes;
