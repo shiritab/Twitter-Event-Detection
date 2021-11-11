5fb1e9cc6e142b579359c6dad6fbe1cd0880fc94
Niv Meiri <nivmeir@post.bgu.ac.il>
2021-11-11 12:23:24 +0200

    updating getevents

--- Frontend/src/pages/EventsPage.vue
+++ Frontend/src/pages/EventsPage.vue
@@ -9,12 +9,20 @@
     locale="en"></b-form-datepicker>
     <br>
     <b-card-group deck>
-        <b-card v-for="event in eventsByValue"
+        <!-- <b-card v-for="event in eventsByValue"
             :key="event.id"
             style="max-width: 20rem; margin: auto;"
             bg-variant="light" class="text-center">
             <router-link :to="{ name: 'event', params:{id:event.id}}">
                 <b-card-text>{{ event.name  }}</b-card-text>
+            </router-link>
+        </b-card> -->
+        <b-card v-for="event in eventsByValue"
+            :key="event.event_name"
+            style="max-width: 20rem; margin: auto;"
+            bg-variant="light" class="text-center">
+            <router-link :to="{ name: 'event', params:{id:event.event_name}}">
+                <b-card-text>{{ event.event_name  }}</b-card-text>
             </router-link>
         </b-card>
     </b-card-group>
@@ -52,6 +60,18 @@ export default {
         ]
       }
     },
+    methods:{
+    async getEvents(){
+      try{
+        const response = await this.axios.get(
+          `http://127.0.0.1:5000/events/summary`
+        );
+        console.log(response)
+        this.events =response.data.events;
+      }catch(error){
+        console.log(error);
+      }
+    }},
     computed:{
         eventsByValue: function(){
             var filteredEvents = this.events.filter((event)=>{
@@ -60,6 +80,10 @@ export default {
             return filteredEvents;
         }
     },
+    created(){
+        console.log("created ")
+        this.getEvents();
+    }
 
 }
 </script>
