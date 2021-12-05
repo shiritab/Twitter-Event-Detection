d1af66fbcdcd2ba842a256cf4757482b8e5ae3b9
shiritab <69760048+shiritab@users.noreply.github.com>
2021-12-05 11:08:22 +0200

    changes in tweet

--- Frontend/src/components/TweetD.vue
+++ Frontend/src/components/TweetD.vue
@@ -24,50 +24,7 @@ export default {
 <style scope>
 
 </style>-->
-<!--<template>
-  <Tweet
-    tweet-id="1370027087818461184"
-    cards="visible"
-    conversation="all"
-    lang="en"
-    theme="light"
-    align="left"
-    :width="400"
-    :dnt="false"
-    @tweet-load-error="onTweetLoadError"
-    @tweet-load-success="onTweetLoadSuccess"
-  >
-    <template v-slot:loading>
-      <span>Loading...</span>
-    </template>
 
-    <template v-slot:error>
-      <span>Sorry, that tweet doesn’t exist!</span>
-    </template>
-  </Tweet>
-</template>
-
-<script>
-import { defineComponent } from 'define-component'
-import Tweet from 'vue-tweet'
-
-export default defineComponent({
-  components: {
-    Tweet
-  },
-  setup() {
-    function onTweetLoadSuccess(embedNode) {
-      console.log(embedNode)
-    }
-
-    function onTweetLoadError() {
-      console.log("Oops... an error has occurred")
-    }
-
-    return { onTweetLoadSuccess, onTweetLoadError }
-  }
-})
-</script>-->
 <template>
   <b-container class="bv-example-row">
     <b-row >
