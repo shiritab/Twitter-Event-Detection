71d58d6420149bb375a0edccfe4e64accd7f4de6
shiritab <69760048+shiritab@users.noreply.github.com>
2022-06-08 11:22:13 +0300

    added about page

--- Frontend/src/pages/AboutPage.vue
+++ Frontend/src/pages/AboutPage.vue
@@ -1,7 +1,78 @@
 <template>
   <div id="About-Page">
       <br>
-      <h1> About </h1>
+      <h1> About Us </h1>
+      <br>
+    <div class="aboutUs">
+      <!-- Niv -->
+      <b-card
+        title="Niv Meiri"
+        img-src="https://media-exp1.licdn.com/dms/image/C4D03AQEDK8MyKrEqgA/profile-displayphoto-shrink_800_800/0/1649338241213?e=1660176000&v=beta&t=4SVWELl4PmoMCvmyvnI-eChU_KBvLFKZPs-V83YGK6U"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>
+          4th year Software and Information Systems Engineering student at Ben Gurion University.
+        </b-card-text>
+
+        <b-button href="https://www.linkedin.com/in/niv-meiri-8b5b6b1b3/" variant="info">About Me</b-button>
+      </b-card>
+
+      <!-- Guy -->
+      <b-card
+        title="Guy Globus"
+        img-src="https://media-exp1.licdn.com/dms/image/C4E03AQFDHAPylYke_Q/profile-displayphoto-shrink_800_800/0/1627581803367?e=1660176000&v=beta&t=kJ7GXSwd1S7DQbJ5zX-RhgUJ1hnU6M3gixbqZhvYHVY"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>
+          4th year Software and Information Systems Engineering student at Ben Gurion University.
+        </b-card-text>
+
+        <b-button href="https://www.linkedin.com/in/guy-globus/" variant="info">About Me</b-button>
+      </b-card>
+    </div>
+    <div class="aboutUs">
+      <!-- Hadasa -->
+      <b-card
+        title="Hadassa Siminovsky"
+        img-src="https://media-exp1.licdn.com/dms/image/C4D03AQFg1LqwLy7Hfw/profile-displayphoto-shrink_800_800/0/1648968788644?e=1660176000&v=beta&t=ucXue4TTPJd8vrbRl87n6rWNOl07YjgRYDdiZvD-h8U"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>
+          4th year Software and Information Systems Engineering student at Ben Gurion University.
+        </b-card-text>
+
+        <b-button href="#" variant="info">About Me</b-button>
+      </b-card>
+
+      <!-- Shiri -->
+      <b-card
+        title="Shiri Tabach"
+        img-src="https://media-exp1.licdn.com/dms/image/C4D03AQEK9_jIeCQG9w/profile-displayphoto-shrink_400_400/0/1652182036440?e=1660176000&v=beta&t=rkUognQzkiPLICZfion2Pcb73TMAfOhxO7s9ATHHUJ8"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>
+          4th year Software and Information Systems Engineering student at Ben Gurion University.
+        </b-card-text>
+
+        <b-button href="#" variant="primary">About Me</b-button>
+      </b-card>
+    </div>
       
   </div>
 </template>
@@ -13,5 +84,10 @@ export default {
 </script>
 
 <style>
-
+.aboutUs {
+  display: flex;
+  margin: 0 auto;
+  justify-content: center;
+  align-content: center;
+}
 </style>
