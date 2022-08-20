9e942b40df41fdc9d5f0f5bf402d12eef633cbf5
hadassaz <hadassaz@post.bgu.ac.il>
2022-08-20 20:54:13 +0300

    remove pictures

--- Frontend/src/pages/AboutPage.vue
+++ Frontend/src/pages/AboutPage.vue
@@ -8,9 +8,6 @@
       <!-- Niv -->
       <b-card
         title="Niv Meiri"
-        img-src="https://media-exp1.licdn.com/dms/image/C4D03AQEDK8MyKrEqgA/profile-displayphoto-shrink_800_800/0/1649338241213?e=1660176000&v=beta&t=4SVWELl4PmoMCvmyvnI-eChU_KBvLFKZPs-V83YGK6U"
-        img-alt="Image"
-        img-top
         tag="article"
         style="max-width: 20rem;"
         class="mb-2"
@@ -25,9 +22,6 @@
       <!-- Guy -->
       <b-card
         title="Guy Globus"
-        img-src="https://media-exp1.licdn.com/dms/image/C4E03AQFDHAPylYke_Q/profile-displayphoto-shrink_800_800/0/1627581803367?e=1660176000&v=beta&t=kJ7GXSwd1S7DQbJ5zX-RhgUJ1hnU6M3gixbqZhvYHVY"
-        img-alt="Image"
-        img-top
         tag="article"
         style="max-width: 20rem;"
         class="mb-2"
@@ -44,9 +38,6 @@
       <!-- Hadasa -->
       <b-card
         title="Hadassa Siminovsky"
-        img-src="https://media-exp1.licdn.com/dms/image/C4D03AQFg1LqwLy7Hfw/profile-displayphoto-shrink_800_800/0/1648968788644?e=1660176000&v=beta&t=ucXue4TTPJd8vrbRl87n6rWNOl07YjgRYDdiZvD-h8U"
-        img-alt="Image"
-        img-top
         tag="article"
         style="max-width: 20rem;"
         class="mb-2"
@@ -61,9 +52,6 @@
       <!-- Shiri -->
       <b-card
         title="Shiri Tabach"
-        img-src="https://media-exp1.licdn.com/dms/image/C4D03AQEK9_jIeCQG9w/profile-displayphoto-shrink_400_400/0/1652182036440?e=1660176000&v=beta&t=rkUognQzkiPLICZfion2Pcb73TMAfOhxO7s9ATHHUJ8"
-        img-alt="Image"
-        img-top
         tag="article"
         style="max-width: 20rem;"
         class="mb-2"
